feac1897683e532caa9c5e0dcb0b287f035b7ffe
JesseKoz <jessemendes2002@gmail.com>
2026-05-23 15:04:04 -0300

    Add report for monitoring active sessions
    Added a new report for monitoring active sessions in the LanHouse, including client names, session start times, and current session values.

--- consultas/relatorios.sql
+++ consultas/relatorios.sql
@@ -36,3 +36,27 @@ WHERE s.IDSESSOES IS NULL;
 | JUNINHO PERNAMBUCANO | JUNINHO123@GMAIL.COM   | 2026-05-16    |
 | CARLINHOS BALA       | CARLINHOS123@GMAIL.COM | 2026-05-17    |
 +----------------------+------------------------+---------------+
+
+
+/* Relatório 03 - Monitoramento de Sessões ativas na LanHouse */
+
+SELECT
+C.NOME,
+S.HORARIO_INICIO,
+fn_valor_sessao(S.IDSESSOES) AS VALOR_ATUAL
+FROM SESSOES S
+JOIN CLIENTES C
+ON C.IDCLIENTE = S.ID_CLIENTE
+WHERE STATUS_SESSAO = 'ABERTA';
+
++--------+---------------------+-------------+
+| NOME   | HORARIO_INICIO      | VALOR_ATUAL |
++--------+---------------------+-------------+
+| PEDRO  | 2026-05-17 17:00:00 |       10.00 |
+| PEDRO  | 2026-05-23 14:43:33 |        NULL |
+| JOAO   | 2026-05-09 11:00:00 |       20.00 |
+| JOAO   | 2026-06-02 09:00:00 |       20.00 |
+| THIAGO | 2026-05-09 15:00:00 |       10.00 |
+| THIAGO | 2026-05-17 11:00:00 |       10.00 |
+| MATEUS | 2026-05-17 13:00:00 |       18.00 |
++--------+---------------------+-------------+
